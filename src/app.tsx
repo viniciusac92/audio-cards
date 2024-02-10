84e14bc8d2bae79c65cdac33c7c6fdcb85429787
viniciusalvesc <vinicius.alves@finlytech.com>
2024-02-10 20:16:27 -0300

    feat: criados cartões e feita componentização

---
+++ src/app.tsx
@@ -0,0 +1,35 @@
+import logo from './assets/logo-nlw-expert.svg';
+import { NewNoteCard } from './components/new-note-card';
+import { NoteCard } from './components/note-card';
+
+export function App() {
+    return (
+        <div className="mx-auto max-w-6xl my-12 space-y-6">
+            <h1 className="text-3xl font-bold underline">
+                <img src={logo} alt="NLW Expert" />
+                Hello world!
+            </h1>
+
+            <form className="w-full">
+                <input
+                    type="text"
+                    placeholder="busque em suas notas"
+                    className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+                ></input>
+            </form>
+
+            <div className="h-px bg-slate-700" />
+
+            <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
+                <NewNoteCard />
+                <NoteCard />
+                <NoteCard />
+                <NoteCard />
+                <NoteCard />
+                <NoteCard />
+                <NoteCard />
+                <NoteCard />
+            </div>
+        </div>
+    );
+}
